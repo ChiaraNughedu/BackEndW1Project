183ad626e897d608c6adc8fd690c70b7e11bd812
ChiaraNughedu <chianughedu@gmail.com>
2025-02-14 12:43:30 +0100

    esercizio terminato

--- BackEndW1Project/Contribuente.cs
+++ BackEndW1Project/Contribuente.cs
@@ -1,7 +1,171 @@
-﻿
-namespace BackEndW1Project
+﻿namespace BackEndW1Project
 {
-    internal class Contribuente
+    public class Contribuente
     {
+        private string nome;
+        private string cognome;
+        private DateTime dataNascita;
+        private string luogoDiNascita;
+        private string codiceFiscale;
+        private char sesso;
+        private string comuneResidenza;
+        private double redditoAnnuale;
+
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            } 
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Inserisci il nome.");
+                nome = value;
+            }
+        }
+
+        public string Cognome
+        {
+            get
+            {
+                return cognome;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Inserisci il cognome");
+                cognome = value;
+            }
+        }
+
+        public DateTime DataNascita
+        {
+            get
+            {
+                return dataNascita;
+            }
+            set
+            {
+                dataNascita = value;
+            }
+        }
+        public string LuogoDiNascita
+        {
+            get
+            {
+                return luogoDiNascita;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Inserisci il luogo di nascita");
+                luogoDiNascita = value;
+            }
+        }
+
+        public string CodiceFiscale
+        {
+            get
+            {
+                return codiceFiscale;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Inserisci Codice Fiscale");
+                codiceFiscale = value;
+            }
+        }
+
+        public char Sesso
+        {
+            get
+            {
+                return sesso;
+            }
+            set
+            {
+                if (value != 'M' && value != 'F')
+                    throw new ArgumentException("Specifica il sesso (M/F)");
+                sesso = value;
+            }
+        }
+
+        public string ComuneResidenza
+        {
+            get
+            {
+                return comuneResidenza;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Inserisci il Comune di Residenza");
+                comuneResidenza = value;
+            }
+        }
+
+        public double RedditoAnnuale
+        {
+            get
+            {
+                return redditoAnnuale;
+            }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Il reddito inserito deve avere un valore valido.");
+                redditoAnnuale = value;
+            }
+        }
+
+        public Contribuente(string nome, string cognome, DateTime dataNascita, string luogoDiNascita, char sesso, string codiceFiscale,  string comuneResidenza, double redditoAnnuale)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            DataNascita = dataNascita;
+            LuogoDiNascita = luogoDiNascita;
+            CodiceFiscale = codiceFiscale;
+            Sesso = sesso;
+            ComuneResidenza = comuneResidenza;
+            RedditoAnnuale = redditoAnnuale;
+        }
+        public double CalcolaImposta()
+        {
+            double imposta = 0;
+            double reddito = RedditoAnnuale;
+
+            while (reddito > 0)
+            {
+                if (reddito > 75000)
+                {
+                    imposta += (reddito - 75000) * 0.43;
+                    reddito = 75000;
+                }
+                else if (reddito > 55000)
+                {
+                    imposta += (reddito - 55000) * 0.41;
+                    reddito = 55000;
+                }
+                else if (reddito > 28000)
+                {
+                    imposta += (reddito - 28000) * 0.38;
+                    reddito = 28000;
+                }
+                else if (reddito > 15000)
+                {
+                    imposta += (reddito - 15000) * 0.27;
+                    reddito = 15000;
+                }
+                else
+                {
+                    imposta += reddito * 0.23;
+                    reddito = 0;
+                }
+            }
+
+            return imposta;
+        }
     }
 }
--- BackEndW1Project/Program.cs
+++ BackEndW1Project/Program.cs
@@ -1,1 +1,126 @@
-﻿
+﻿using BackEndW1Project;
+
+        try
+        {
+            string nome = InserisciDato("Nome");
+            string cognome = InserisciDato("Cognome");
+            DateTime dataNascita = InserisciData("Data di nascita");
+            string luogoDiNascita = InserisciDato("Luogo di Nascita");
+            string codiceFiscale = InserisciDato("Codice Fiscale");
+            char sesso = InserisciSesso("Sesso");
+            string comuneResidenza = InserisciDato("Comune di Residenza");
+            double redditoAnnuale = InserisciReddito("Reddito Annuale");
+
+            
+            Contribuente contribuente = new Contribuente(nome, cognome, dataNascita, luogoDiNascita, sesso, codiceFiscale, comuneResidenza, redditoAnnuale);
+
+            double imposta = contribuente.CalcolaImposta();
+
+            Console.WriteLine("\nCALCOLO IMPOSTA DA VERSARE");
+            Console.WriteLine($"Contribuente: {contribuente.Nome} {contribuente.Cognome};");
+            Console.WriteLine($"Nato il: {contribuente.DataNascita:dd/MM/yyyy} a {contribuente.LuogoDiNascita};");
+            Console.WriteLine($"Sesso: {contribuente.Sesso};");
+            Console.WriteLine($"Residente a: {contribuente.ComuneResidenza};");
+            Console.WriteLine($"Codice Fiscale: {contribuente.CodiceFiscale};");
+            Console.WriteLine($"Reddito dichiarato: {contribuente.RedditoAnnuale:F2};");
+            Console.WriteLine($"Imposta da versare: {imposta:F2};");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Errore: {ex.Message}");
+        }
+    
+
+    static string InserisciDato(string campo)
+    {
+        string valore;
+        do
+        {
+            Console.Write($"Inserisci il {campo}: ");
+            valore = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(valore))
+            {
+                Console.WriteLine($"{campo} non può essere vuoto. Riprova.");
+            }
+        } while (string.IsNullOrWhiteSpace(valore));
+
+        return valore;
+    }
+static DateTime InserisciData(string campo)
+{
+    DateTime data;
+    while (true)
+    {
+        try
+        {
+            Console.Write($"Inserisci {campo}: ");
+            string input = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                throw new ArgumentException("Inserire la data di nascita.");
+            }
+
+            data = DateTime.Parse(input);
+            break;
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Formato data non valido. Usa il formato DD/MM/YYYY.");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message); 
+        }
+    }
+    return data;
+}
+static char InserisciSesso(string campo)
+    {
+        char sesso;
+        while (true)
+        {
+            try
+            {
+                Console.Write($"Inserisci {campo}: ");
+                sesso = Char.ToUpper(Console.ReadLine()[0]);
+                if (sesso != 'M' && sesso != 'F')
+                {
+                    throw new ArgumentException("Valore non valido. Inserisci 'M' o 'F'.");
+                }
+                break;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        return sesso;
+    }
+
+    static double InserisciReddito(string campo)
+    {
+        double reddito;
+        while (true)
+        {
+            try
+            {
+                Console.Write($"Inserisci {campo}: ");
+                reddito = double.Parse(Console.ReadLine());
+                if (reddito < 0)
+                {
+                    throw new ArgumentException("Il reddito non può essere negativo. Riprova.");
+                }
+                break;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Formato del reddito non valido. Inserisci un numero valido.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        return reddito;
+    }
